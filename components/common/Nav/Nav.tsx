b2a98ad6ea91f0bc6a417170153a5596067eea7b
Edgar López <edgar.lr.18@gmail.com>
2021-01-22 21:17:19 -0600

    Add nav

--- components/common/Nav/Nav.tsx
+++ components/common/Nav/Nav.tsx
@@ -2,35 +2,28 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import cn from 'classnames'
 import { useHideOnScroll } from '@lib/hooks/use-hide-on-scroll'
+import s from './Nav.module.css'
 
 const Nav = ({ categories }: { categories: TCategory[] }) => {
   const router = useRouter()
   const { isHidden } = useHideOnScroll()
   return (
     <nav
-      className={cn(
-        'overflow-x-scroll sticky flex whitespace-nowrap px-4 top-14 z-10 bg-primary scrollbar-none transform transition-transform duration-300',
-        'md:justify-center',
-        isHidden ? '-translate-y-full' : 'translate-y-0'
-      )}
+      className={cn(s.root, 'scrollbar-none ', {
+        [s.hide]: isHidden,
+      })}
     >
       <Link href={`/`}>
-        <a
-          className={cn(
-            'uppercase px-4 pt-1 pb-2 text-sm font-bold',
-            router.pathname === '/' && 'border-b-2'
-          )}
-        >
+        <a className={cn(s.link, { [s.active]: router.pathname === '/' })}>
           HOME
         </a>
       </Link>
       {categories.map((category) => (
         <Link href={`/${category.slug}`} key={category.slug}>
           <a
-            className={cn(
-              'uppercase pt-1 pb-2 px-4 text-sm font-bold ',
-              router.query.slug === category.slug && 'border-b-2'
-            )}
+            className={cn(s.link, {
+              [s.active]: router.query.slug === category.slug,
+            })}
           >
             {category.title}
           </a>
