8dca6f4442c3384b38cf81f520e1bcfe01a399cc
Edgar López <edgar.lr.18@gmail.com>
2021-01-29 15:42:04 -0600

    Fix 404 page colors

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,9 @@ export default function custom404() {
       </Link>
 
       <div className="w-9/12 text-center block mt-1/3 mx-auto">
-        <h1 className="text-8xl text-accent my-8">404</h1>
+        <h1 className="text-8xl my-8 text-blue-light">
+          4<span className="text-blue opacity-80">0</span>4
+        </h1>
         <p className="font-bold text-lg">Página no encontrada :c</p>
         <span className="text-primary-300 text-sm">
           Revisa la url o intenta de nuevo
