fa4f443302ad3b4f6e6c955dcb15d2df94089995
Edgar López <edgar.lr.18@gmail.com>
2021-01-29 15:16:03 -0600

    Add scroll indicator

--- components/article/ArticlesCarousel/ArticlesCarousel.tsx
+++ components/article/ArticlesCarousel/ArticlesCarousel.tsx
@@ -1,11 +1,14 @@
 import ArticleCardCarousel from '../ArticleCard/ArticleCardCarousel'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import cn from 'classnames'
+import s from './ArticlesCarousel.module.css'
 
 type Props = {
   articles: TArticle[]
 }
 
 const ArticlesCarousel = ({ articles }: Props) => {
+  const [scrollProgress, setScrollProgress] = useState(0)
   const carouselRef = useRef<HTMLDivElement>(null)
 
   // const scrollToLeft = (e: MouseEvent<HTMLButtonElement>) => {
@@ -17,6 +20,40 @@ const ArticlesCarousel = ({ articles }: Props) => {
   //   e.preventDefault()
   //   carouselRef.current?.scrollBy({ left: 1, top: 0, behavior: 'smooth' })
   // }
+
+  useEffect(() => {
+    if (!carouselRef.current) return
+
+    const element = carouselRef.current
+
+    const scrollListener = () => {
+      const windowScroll = element.scrollLeft
+      const totalWidth = element.scrollWidth - element.clientWidth
+
+      if (windowScroll === 0) {
+        return setScrollProgress(0)
+      }
+      if (windowScroll > totalWidth) {
+        return setScrollProgress(100)
+      }
+      return setScrollProgress((windowScroll / totalWidth) * 100)
+    }
+
+    element.addEventListener('touchmove', scrollListener)
+    return () => element.removeEventListener('touchmove', scrollListener)
+  }, [])
+
+  const renderScrollIndicator = () => {
+    const selected = (scrollProgress * articles.length) / 100
+    return articles.map((article, index) => (
+      <div
+        className={cn('w-full mx-1 bg-primary-40 pb-1 rounded-sm', {
+          [s.active]: selected >= index && selected <= index + 1,
+        })}
+        key={article.slug}
+      ></div>
+    ))
+  }
 
   return (
     <section className="mt-6 mb-16">
@@ -30,12 +67,7 @@ const ArticlesCarousel = ({ articles }: Props) => {
       </div>
 
       <div className="flex justify-between h-auto mt-12 ">
-        {articles.map((article) => (
-          <div
-            className="w-full mx-1 bg-primary pb-1 rounded-sm"
-            key={article.slug}
-          ></div>
-        ))}
+        {renderScrollIndicator()}
         {/* <ul className="flex">
           <li>
             <Button onClick={scrollToLeft} ariaLabel="Previus article">
