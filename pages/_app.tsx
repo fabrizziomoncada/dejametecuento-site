4f6606aa6fd4276c42c8362634d464b36d7aec07
Edgar López <edgar.lr.18@gmail.com>
2021-02-01 17:04:04 -0600

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,27 @@ import '@styles/utilities.css'
 import Head from '@components/common/head'
 import ToastProvider from '@components/ui/Toast/ToastProvider'
 import ListProvider from '@components/common/ListProvider'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '@lib/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Only run GA on Production
+    if (process.env.NODE_ENV !== 'production') return
+
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider>
       <ToastProvider>
